76c280c74e02d7c1a8c74fd824be55e055b79f09
devsahadat <cse.devsahadat@gmail.com>
2022-01-28 01:01:12 +0600

    Manage User Show done

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,12 @@ Route::get('contact-us','Frontend\FrontendController@contactUs')->name('contact.
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('users')->group(function(){
+    Route::get('/view','Backend\UserController@view')->name('users.view');
+    Route::get('/add','Backend\UserController@add')->name('users.add');
+    Route::post('/store','Backend\UserController@store')->name('users.store');
+    Route::get('/edit/{id}','Backend\UserController@edit')->name('users.edit');
+    Route::post('/update/{id}','Backend\UserController@update')->name('users.update');
+    Route::get('/delete/{id}','Backend\UserController@delete')->name('users.delete');
+});
